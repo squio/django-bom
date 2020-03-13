c759b87e1f2d4ae2415414f54da3acd6dcd4219f
Johannes la Poutre <johannes@truekinetix.com>
2020-03-13 14:35:56 +0100

    Make app name in templates configurable in settings

--- bom/context_processors.py
+++ bom/context_processors.py
@@ -3,7 +3,13 @@ from django.conf import settings  # import the settings file
 
 def bom_config(request):
     base_template = 'bom/base.html'
+    app_name = 'IndaBOM'
     if settings.BOM_CONFIG:
         if 'base_template' in settings.BOM_CONFIG:
             base_template = settings.BOM_CONFIG['base_template']
-    return {'BASE_TEMPLATE': base_template}
+        if 'app_name' in settings.BOM_CONFIG:
+            app_name = settings.BOM_CONFIG['app_name']
+    return {
+        'BASE_TEMPLATE': base_template,
+        'APP_NAME': app_name,
+    }
